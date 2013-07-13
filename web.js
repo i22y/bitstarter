1f5ae501b861a9a3fb1f5a77c582960eb681b1dc
i22y <israelpalmerin@gmail.com>
2013-07-13 00:21:39 +0000

    Rollback to previous version

--- web.js
+++ web.js
@@ -3,8 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var my_index = fs.readFileSync("index.html");
-    response.send(my_index.toString('utf-8'));
+  response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
